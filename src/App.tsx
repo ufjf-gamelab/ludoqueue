0cbf6c416ee2ce938277e25cfd58b9b4d18f9084
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-05-19 16:00:06 -0300

    troca para arrowfunction

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [nodes, setNodes] = useState(new Map(
-    [["Apple", 4.75], 
-    ["Grape", 2.72],
-    ["Banana", 1.00]]
-  )
-    );
-    nodes.set("Durian",13);
+  const [nodes, setNodes] = useState(
+    new Map([
+      ["Apple", 4.75],
+      ["Grape", 2.72],
+      ["Banana", 1.0],
+    ])
+  );
+  nodes.set("Durian", 13);
   return (
     <>
       <h1>Vite + React</h1>
@@ -18,9 +19,11 @@ function App() {
           count is {count}
         </button>
         <ul>
-          {Array.from(nodes.entries()).map(function (item){
-            return <li>{item[0]} &rarr; {item[1]}</li>
-          })}
+          {Array.from(nodes.entries()).map(([key, value]) => (
+            <li>
+              {key} &rarr; {value}
+            </li>
+          ))}
         </ul>
       </div>
     </>
