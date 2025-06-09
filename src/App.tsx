91a1fa14731233a4832c7c365de59b77dbace79b
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-06-09 15:37:39 -0300

    adiciona nodetype

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import { useState } from "react";
 import "./App.css";
 
+type NodeType = {
+  id: string;
+  name: string;
+  val: number;
+};
+
 type GraphType = {
-  nodes: string[];
+  nodes: NodeType[];
   connections: [string, string][];
-}
+};
 const g: GraphType = {
-  nodes: ["Apple", "Grape", "Banana"],
-  connections: [
-    ["Apple", "Grape"],
-    ["Apple", "Banana"],
-    ["Grape", "Banana"],
-    ["Banana", "Apple"],
-    ["Banana", "Cashew"],
+  nodes: [
+    { id: "apple", name: "Apple", val: 0 },
+    { id: "grape", name: "Grape", val: 0 },
+    { id: "banana", name: "Banana", val: 0 },
   ],
-}
+  connections: [
+    ["apple", "grape"],
+    ["apple", "banana"],
+    ["grape", "banana"],
+    ["banana", "apple"],
+    ["banana", "cashew"],
+  ],
+};
 function App() {
   const [graphData, setGraphData] = useState<GraphType>(g);
   const adjacencyList = createAdjacencyList(graphData);
@@ -26,19 +36,22 @@ function App() {
         <button
           onClick={() => {
             const isPresent = graphData.connections.some(([from, to]) => {
-              return from === "Cashew" && to === "Durian" || from === "Durian" && to === "Cashew";
+              return (
+                (from === "Cashew" && to === "Durian") ||
+                (from === "Durian" && to === "Cashew")
+              );
             });
             if (isPresent) return;
             graphData.connections.push(["Durian", "Cashew"]);
-            setGraphData({...graphData});
+            setGraphData({ ...graphData });
           }}
         >
           click me
         </button>
         <h2>Nodes</h2>
         <ul>
-          {graphData.nodes.map((nodeID) => (
-            <li>{nodeID}</li>
+          {graphData.nodes.map((node) => (
+            <li>{node.id}</li>
           ))}
         </ul>
         <h2>Connections</h2>
@@ -70,7 +83,7 @@ function App() {
 function createAdjacencyList(graphData: GraphType) {
   const adj: Map<string, string[]> = new Map();
   graphData.nodes.forEach((node) => {
-    adj.set(node, []);
+    adj.set(node.id, []);
   });
   graphData.connections.forEach(([from, to]) => {
     if (!adj.get(from)) {
