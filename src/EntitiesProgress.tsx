b254c9dc3e15e6fbde4f9bfa61a3953d63aedf10
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-08-22 13:29:05 -0300

    css entitesprogress

--- src/EntitiesProgress.tsx
+++ src/EntitiesProgress.tsx
@@ -2,22 +2,40 @@ import type { GameType } from "./types";
 import React from "react";
 
 export default function EntitiesProgress({ game }: { game: GameType }) {
-  const entitiesList: React.ReactElement[] = []; // conferir 
+  const entitiesList: React.ReactElement[] = []; // conferir
 
   game.entities.forEach((entity) => {
     entitiesList.push(
-      <div key={entity.id} className="entity-item">
-        <div>{entity.id}</div>
+      <div
+        key={entity.id}
+        style={{
+          background: "rgba(0, 0, 0, 0.1)",
+          minWidth: "120px",
+        }}
+      >
+        <div style={{ marginBottom: "5px" }}>{entity.id}</div>
         <progress
-            value = {entity.val}
-            max = {entity.max}
+          value={entity.val}
+          max={entity.max}
+          style={{ width: "100%" }}
         />
-        <div>
+        <div style={{ marginTop: "5px", textAlign: "center" }}>
           {entity.val} / {entity.max}
         </div>
       </div>
     );
   });
 
-  return <div className="entities-progress">{entitiesList}</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        gap: "10px",
+        margin: "5px",
+      }}
+    >
+      {entitiesList}
+    </div>
+  );
 }
