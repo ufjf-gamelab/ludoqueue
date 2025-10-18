2e6e0a0269c641b65a69af79a1da11e94ec3958f
Igor Knop <igorknop@gmail.com>
2025-10-18 15:39:47 -0300

    add initial good transport animation

--- src/entities/Transport/TransporterTile.tsx
+++ src/entities/Transport/TransporterTile.tsx
@@ -4,10 +4,12 @@ import "./TransporterTile.css";
 import { TransportIcons } from "../Icons";
 
 export default function TransporterTile({ entity }: { entity: EntityTransportType }) {
+  const spanKey = String(Math.random());
   return (
     <div className="transporter-tile">
       {TransportIcons[entity.direction]}
       <progress value={entity.val} max={entity.max} title={`${entity.val}/${entity.max}`} />
+      <span key={spanKey} className={["transported-good", entity.direction].join(' ')} aria-hidden="true" />
     </div>
   );
 }
