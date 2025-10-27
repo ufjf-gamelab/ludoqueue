0a39c1303ff1f2c55468cc27e896198228a9927f
Rebecca Fiorillo <estevaovrb2014@gmail.com>
2025-10-27 10:59:15 -0300

    coloca condição nas animações

--- src/entities/Assets.tsx
+++ src/entities/Assets.tsx
@@ -3,5 +3,12 @@ import "./Assets.css";
 
 //Transport assets
 export function EntitySprites(entity: EntityType) {
-    return <div className={`${entity.type}-sprite ${entity.type=="transport" ? entity.direction : ""}`}></div>;
+    let isPlaying: boolean = false;
+    if (entity.type === "source" && entity.val < entity.max) {
+        isPlaying = true;
+    }
+    if (entity.type === "transport" && entity.val > 0) {
+        isPlaying = true;
+    }
+    return <div className={`${entity.type}-sprite ${entity.type=="transport" ? entity.direction : ""} ${isPlaying ? "running" : "cooldown"}`}></div>;
 }
