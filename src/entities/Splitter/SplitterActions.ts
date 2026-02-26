dc1023dce58e024255b14cd034bf9ccb376c890e
Rebecca Fiorillo <estevaovrb2014@gmail.com>
2026-02-26 14:59:31 -0300

    update splitter correto (apenas para cima)

--- src/entities/Splitter/SplitterActions.ts
+++ src/entities/Splitter/SplitterActions.ts
@@ -31,7 +31,7 @@ export function createSplitter(
 ) {
   if (
     Array.from(state.entities.values()).find(
-      (entity) => entity.x === x && entity.y === y
+      (entity) => entity.x === x && entity.y === y,
     )
   ) {
     //checagem se ja existe entidade na posicao
@@ -84,12 +84,14 @@ export function deleteSplitter(state: GameType, splitter: string) {
 export function changeSplitterEntryDirection(
   state: GameType,
   splitterID: string,
-  direction: DirectionType
+  direction: DirectionType,
 ) {
   const splitterEntity = state.entities.get(splitterID);
   if (splitterEntity && splitterEntity.type === "splitter") {
     const newState = structuredClone(state);
-    const newSplitterEntity = newState.entities.get(splitterID) as EntitySplitterType;
+    const newSplitterEntity = newState.entities.get(
+      splitterID,
+    ) as EntitySplitterType;
     newSplitterEntity.entryDirection = direction;
     updateSplitterConnections(newState, newSplitterEntity);
     return newState;
@@ -99,58 +101,141 @@ export function changeSplitterEntryDirection(
 
 export function updateSplitterConnections(
   state: GameType,
-  splitter: EntitySplitterType
+  splitter: EntitySplitterType,
 ) {
   splitter.source = null;
   splitter.target = null;
   switch (splitter.entryDirection) {
-    case "up":{
-      const sourceID = Array.from(state.entities.values()).find(
-        (entity) =>
-          entity.x === splitter.x && entity.y === splitter.y - 1
-      )?.id;
-      if (sourceID){splitter.source = sourceID};
-      const sourceEntity = sourceID ? state.entities.get(sourceID) : null;
-      if (sourceEntity && sourceEntity.type === "transport") {
-        const transportEntity = sourceEntity;
-        transportEntity.target = splitter.id;
-      }
-      //achar targets 
-      const target0 = Array.from(state.entities.values()).find(
-        (entity) =>          entity.x === splitter.x + 1 && entity.y === splitter.y //direita
-      )?.id;
-      const target0Entity = target0 ? state.entities.get(target0) : null;
-      if (target0Entity && target0Entity.type === "transport") {
-        const transportEntity = target0Entity;
-        transportEntity.source = splitter.id;
-      }
-      const target1 = Array.from(state.entities.values()).find(
-        (entity) =>          entity.x === splitter.x && entity.y === splitter.y + 1 //baixo
-      )?.id;
-      const target1Entity = target1 ? state.entities.get(target1) : null;
-      if (target1Entity && target1Entity.type === "transport") {
-        const transportEntity = target1Entity;
-        transportEntity.source = splitter.id;
-      }
-      const target2 = Array.from(state.entities.values()).find(
-        (entity) =>          entity.x === splitter.x - 1 && entity.y === splitter.y //esquerda
-      )?.id;
-      const target2Entity = target2 ? state.entities.get(target2) : null;
-      if (target2Entity && target2Entity.type === "transport") {
-        const transportEntity = target2Entity;
-        transportEntity.source = splitter.id;
-      }
-      if (target0){
-        splitter.target = [target0];
-      }
-      if (target1){
-        splitter.target = [...(splitter.target || []), target1];
-      }
-      if (target2){
-        splitter.target = [...(splitter.target || []), target2];
-      }
-      
+    case "up": {
+      updateSplitterUpperEntry(state, splitter);
       break;
     }
+    case "down": {
+      //updateSplitterDownEntry(state, splitter);
+      break;
+    }
+    case "left": {
+      //updateSplitterLeftEntry(state, splitter);
+      break;
+    }
+    case "right": {
+      //updateSplitterRightEntry(state, splitter);
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+}
+
+function updateSplitterUpperEntry(
+  state: GameType,
+  splitter: EntitySplitterType,
+) {
+  let sourceID =
+    Array.from(state.entities.values()).find(
+      (entity) => entity.x === splitter.x && entity.y === splitter.y - 1,
+    )?.id ?? null;
+  let sourceEntity = sourceID ? state.entities.get(sourceID) : null;
+  if (
+    sourceEntity &&
+    ((sourceEntity.type === "transport" &&
+      sourceEntity.leavingDirection !== "down") ||
+      (sourceEntity.type === "stock" && sourceEntity.direction !== "down") ||
+      (sourceEntity.type === "source" &&
+        sourceEntity.leavingDirection !== "down") ||
+      (sourceEntity.type === "splitter" &&
+        sourceEntity.entryDirection !== "up") ||
+      sourceEntity.type === "consumer")
+  ) {
+    sourceEntity = null;
+    sourceID = null;
+  }
+  if (sourceID) {
+    splitter.source = sourceID;
+  }
+  if (
+    sourceEntity &&
+    sourceEntity.type === "transport" &&
+    sourceEntity.leavingDirection === "down"
+  ) {
+    const transportEntity = sourceEntity;
+    transportEntity.target = splitter.id;
+  }
+  //achar targets
+  let target0 = Array.from(state.entities.values()).find(
+    (entity) => entity.x === splitter.x + 1 && entity.y === splitter.y, //direita
+  )?.id ?? null;
+  let target0Entity = target0 ? state.entities.get(target0) : null;
+  if (
+    target0Entity &&
+    ((target0Entity.type === "transport" &&
+      target0Entity.entryDirection !== "left") ||
+      (target0Entity.type === "stock" && target0Entity.direction !== "right") ||
+      (target0Entity.type === "consumer" &&
+        target0Entity.entryDirection !== "left") ||
+      (target0Entity.type === "splitter" &&
+        target0Entity.entryDirection !== "left") ||
+      (target0Entity.type === "source"))
+  ) {
+    target0Entity = null;
+    target0 = null;
+  }
+  if (target0Entity && target0Entity.type === "transport") {
+    const transportEntity = target0Entity;
+    transportEntity.source = splitter.id;
+  }
+  let target1 = Array.from(state.entities.values()).find(
+    (entity) => entity.x === splitter.x && entity.y === splitter.y + 1, //baixo
+  )?.id?? null;
+  let target1Entity = target1 ? state.entities.get(target1) : null;
+  if (
+    target1Entity &&
+    ((target1Entity.type === "transport" &&
+      target1Entity.entryDirection !== "up") ||
+      (target1Entity.type === "stock" && target1Entity.direction !== "down") ||
+      (target1Entity.type === "consumer" &&
+        target1Entity.entryDirection !== "up") ||
+      (target1Entity.type === "splitter" &&
+        target1Entity.entryDirection !== "up") ||
+      (target1Entity.type === "source"))
+  ) {
+    target1Entity = null;
+    target1 = null;
+  }
+  if (target1Entity && target1Entity.type === "transport") {
+    const transportEntity = target1Entity;
+    transportEntity.source = splitter.id;
+  }
+  let target2 = Array.from(state.entities.values()).find(
+    (entity) => entity.x === splitter.x - 1 && entity.y === splitter.y, //esquerda
+  )?.id?? null;
+  let target2Entity = target2 ? state.entities.get(target2) : null;
+  if (
+    target2Entity &&
+    ((target2Entity.type === "transport" &&
+      target2Entity.entryDirection !== "right") ||
+      (target2Entity.type === "stock" && target2Entity.direction !== "left") ||
+      (target2Entity.type === "consumer" &&
+        target2Entity.entryDirection !== "right") ||
+      (target2Entity.type === "splitter" &&
+        target2Entity.entryDirection !== "right") ||
+      (target2Entity.type === "source"))
+  ) {
+    target2Entity = null;
+    target2 = null;
+  }
+  if (target2Entity && target2Entity.type === "transport") {
+    const transportEntity = target2Entity;
+    transportEntity.source = splitter.id;
+  }
+  if (target0) {
+    splitter.target = [target0];
+  }
+  if (target1) {
+    splitter.target = [...(splitter.target || []), target1];
+  }
+  if (target2) {
+    splitter.target = [...(splitter.target || []), target2];
   }
 }
