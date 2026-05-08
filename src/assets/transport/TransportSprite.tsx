fc919c4fece38fbf83481f6039e71d7a9623de48
Rebecca Fiorillo <estevaovrb2014@gmail.com>
2026-05-07 22:36:54 -0300

    faz transport tocar sempre

--- src/assets/transport/TransportSprite.tsx
+++ src/assets/transport/TransportSprite.tsx
@@ -4,16 +4,16 @@ import type { EntityTransportType } from "../../entities/EntitiesTypes";
 
 export function TransportSprite({ entity }: { entity: EntityTransportType }) {
   const [animating, setAnimating] = useState(false);
+
   useEffect(() => {
-    if (entity.movingGoods.length === 0) return;
     setAnimating(false);
     const timeout = setTimeout(() => setAnimating(true), 10);
     return () => clearTimeout(timeout);
-  }, [entity.movingGoods.length]);
+  }, [entity.goods]);
 
   return (
     <div
-      className={`transport-sprite ${["transport-direction", entity.entryDirection, entity.leavingDirection, animating? "animate" : ""].join("-")}`}
+      className={`transport-sprite ${["transport-direction", entity.entryDirection, entity.leavingDirection, animating ? "animate" : ""].join("-")}`}
     />
   );
 }
